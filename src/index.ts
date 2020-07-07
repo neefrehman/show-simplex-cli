dac75ecbd573601da02cc714d7e74ce8690860cf
Neef Rehman <neef.rehman@gmail.com>
2020-07-07 19:19:04 +0100

    feature: add block and angle displays

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
+import { program } from "commander";
 import palettes from "nice-color-palettes";
 import chalk from "chalk";
 import { noise3D, pickRandom, shuffle, mapRange } from "./utils";
+
+program
+    .version("1.0.0")
+    .description("Displays an animated simplex noise field in your terminal")
+    .option("-a, --angle", "outputs noise field as angles")
+    .option("-b, --block", "outputs noise field as colour blocks")
+    .parse(process.argv);
 
 const clearScrollback = (): boolean =>
     process.stdout.write("\u001b[H\u001b[2J\u001b[3J");
@@ -36,24 +44,50 @@ const startDrawLoop = (pointPositions: [number, number][]): void => {
             const [y, x] = position;
             const z = noise3D(x, y, noiseZ);
 
-            const nChars = 9;
-            const zMult = Math.abs(Math.floor(z * nChars));
-            const colourMap = Math.floor(
-                mapRange(zMult, 4, nChars - 1, 0, nColours - 1)
-            );
-
             let char = " ";
-            if ([0, 1, 2, 3].includes(zMult)) char = " ";
-            if (zMult === 4) char = ".";
-            if (zMult === 5) char = "-";
-            if (zMult === 6) char = "o";
-            if (zMult === 7) char = "+";
-            if (zMult === 8) char = "x";
-            if (zMult === 9) char = "X";
+            let color: string;
 
-            const color = randomPalette[colourMap];
+            if (program.block) {
+                const nChars = 5;
+                const zMult = Math.abs(Math.floor(z * nChars));
+                const colourMap = Math.floor(
+                    mapRange(zMult, 0, nChars - 1, 0, nColours - 1)
+                );
 
-            process.stdout.write(chalk.hex(color)(char));
+                color = randomPalette[colourMap];
+                process.stdout.write(chalk.bgHex(color)(char));
+            } else if (program.angle) {
+                const nChars = 4;
+                const zMult = Math.abs(Math.floor(z * nChars));
+                const colourMap = Math.floor(
+                    mapRange(zMult, 0, nChars - 1, 0, nColours - 1)
+                );
+
+                if (zMult === 0) char = "|";
+                if (zMult === 1) char = "/";
+                if (zMult === 2) char = "—";
+                if (zMult === 3) char = "\\";
+
+                color = randomPalette[colourMap];
+                process.stdout.write(chalk.hex(color)(char));
+            } else {
+                const nChars = 9;
+                const zMult = Math.abs(Math.floor(z * nChars));
+                const colourMap = Math.floor(
+                    mapRange(zMult, 4, nChars - 1, 0, nColours - 1)
+                );
+
+                if ([0, 1, 2, 3].includes(zMult)) char = " ";
+                if (zMult === 4) char = ".";
+                if (zMult === 5) char = "-";
+                if (zMult === 6) char = "o";
+                if (zMult === 7) char = "+";
+                if (zMult === 8) char = "x";
+                if (zMult === 9) char = "X";
+
+                color = randomPalette[colourMap];
+                process.stdout.write(chalk.hex(color)(char));
+            }
 
             noiseZ += noiseZVel;
         });
